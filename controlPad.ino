09735dd65e6c9ed2316087d35df5b6451bcb9b49
JasenDX <JasonDM1213@gmail.com>
2023-12-17 20:53:04 -0500

    began writing

---
+++ controlPad.ino
@@ -0,0 +1,13 @@
+/*
+    Author: Jason Mele
+    Date Created: 12/17/2023
+    Purpose: Translate the input from a Game Boy Zero DMG-01 6 button PCB to ASCII so it functions as a keyboard & can be read by the Pi
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
